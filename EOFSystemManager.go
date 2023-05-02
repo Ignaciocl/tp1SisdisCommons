3b89f57f4f54457b62cd12861e77af8d157ef028
ignacio lugones <ignacio.lugones@wibson.org>
2023-05-02 02:45:55 -0300

    continue dev

---
+++ EOFSystemManager.go
@@ -0,0 +1,115 @@
+package commons
+
+import (
+	"encoding/json"
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"net"
+)
+
+type ValidatorStillUsingData interface {
+	isStillUsingNecessaryDataForFile(file string, city string) bool
+	shouldExitAfterEOF() bool
+}
+
+type WaitForEof interface {
+	AnswerEofOk(value ValidatorStillUsingData) // blocking method
+	Close()
+}
+
+type triggerEOF struct {
+	File    string `json:"file"`
+	City    string `json:"city"`
+	Sigterm bool   `json:"sigterm"`
+}
+
+type answerEofOk struct {
+	addr            string
+	conn            *amqp.Connection
+	ch              *amqp.Channel
+	msgs            <-chan amqp.Delivery
+	finish          chan struct{}
+	finishedHearing chan struct{}
+}
+
+func (a *answerEofOk) AnswerEofOk(value ValidatorStillUsingData) {
+	go func() {
+		var trigger triggerEOF
+		for d := range a.msgs {
+			if err := json.Unmarshal(d.Body, &trigger); err != nil {
+				FailOnError(err, "could not understand message")
+			}
+			if value.isStillUsingNecessaryDataForFile(trigger.File, trigger.City) {
+				a.sendEOFCorrect()
+			}
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	data := <-a.finish
+	a.finishedHearing <- data
+}
+
+func (a *answerEofOk) Close() {
+	a.Close()
+	a.ch.Close()
+}
+
+func (a *answerEofOk) sendEOFCorrect() {
+	udpAddr, err := net.ResolveUDPAddr("udp", a.addr)
+	if err != nil {
+		FailOnError(err, "could not parse address")
+	}
+	conn, _ := net.DialUDP("udp", nil, udpAddr)
+	<-a.finishedHearing
+	conn.Write([]byte("ok"))
+	conn.Close()
+}
+
+func CreateConsumerEOF(connection string, queueType string) (WaitForEof, error) {
+	url := fmt.Sprintf("amqp://guest:guest@%s:5672/", connection)
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		FailOnError(err, "Failed to connect to RabbitMQ")
+		conn.Close()
+		return nil, err
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		FailOnError(err, "Failed to connect to RabbitMQ")
+		ch.Close()
+		conn.Close()
+		return nil, err
+	}
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	FailOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,    // queue name
+		"",        // routing key
+		queueType, // exchange
+		false,
+		nil,
+	)
+	FailOnError(err, "Failed to bind a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	FailOnError(err, "Failed to register a consumer")
+	return &answerEofOk{ch: ch, conn: conn, msgs: msgs, finish: make(chan struct{}, 1), finishedHearing: make(chan struct{}, 1)}, nil
+}
---
+++ Publisher.go
@@ -0,0 +1,65 @@
+package commons
+
+import (
+	"context"
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Publisher interface {
+	Publish(targetPublic string, body []byte) error
+	Close()
+}
+
+type publisher struct {
+	conn *amqp.Connection
+	ch   *amqp.Channel
+}
+
+func (p *publisher) Publish(targetPublic string, body []byte) error {
+	err := p.ch.ExchangeDeclare(
+		targetPublic, // name
+		"fanout",     // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	FailOnError(err, "Failed to declare an exchange")
+	ctx := context.Background()
+	return p.ch.PublishWithContext(ctx,
+		"", // exchange
+		"",
+		false, // mandatory
+		false, // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        body,
+		},
+	)
+}
+
+func (p *publisher) Close() {
+	p.Close()
+	p.ch.Close()
+}
+
+func CreatePublisher(connection string) (Publisher, error) {
+
+	url := fmt.Sprintf("amqp://guest:guest@%s:5672/", connection)
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		FailOnError(err, "Failed to connect to RabbitMQ")
+		conn.Close()
+		return nil, err
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		FailOnError(err, "Failed to connect to RabbitMQ")
+		ch.Close()
+		conn.Close()
+		return nil, err
+	}
+	return &publisher{ch: ch, conn: conn}, nil
+}
