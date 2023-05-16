dda715d697a8a8f3ca94d1a21ee631c763d914de
ignacio lugones <ignacio.lugones@wibson.org>
2023-05-16 17:46:59 -0300

    add minor improvements over the commons

---
+++ utils.go
@@ -0,0 +1,32 @@
+package commons
+
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func WaitForSigterm(manager GracielaManager) {
+	oniChan := make(chan os.Signal, 1)
+	// catch SIGETRM or SIGINTERRUPT
+	signal.Notify(oniChan, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		if manager != nil {
+			manager.WaitForSigterm()
+			oniChan <- syscall.SIGTERM
+		}
+	}()
+	<-oniChan
+}
+
+func RecoverFromPanic(manager GracielaManager, connection string) {
+	if manager == nil {
+		manager, _ = CreateGracefulManager(connection)
+	}
+	if r := recover(); r != nil {
+		log.Infof("recovered from panic at %v", r)
+		manager.SignalSigterm()
+	}
+
+}
