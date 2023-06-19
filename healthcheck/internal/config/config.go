2c340043dc1b402c32c5e52077f57477fdf96d34
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2023-06-19 05:03:43 -0300

    change path

--- healthcheck/internal/config/config.go
+++ healthcheck/internal/config/config.go
@@ -6,7 +6,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const configFilepath = "./config.yaml"
+const configFilepath = ".config/config.yaml"
 
 type HealthCheckConfig struct {
 	Port               int    `yaml:"healthcheck_port"`
