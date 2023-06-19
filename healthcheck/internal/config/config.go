2af2a88b6909e81eb187719d290da33190a9b4c3
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2023-06-19 05:30:44 -0300

    change path again lpm que lo remil pario, es mas facil,tener esto en un puto utils en vez de estar taggeando 300 veces

--- healthcheck/internal/config/config.go
+++ healthcheck/internal/config/config.go
@@ -6,7 +6,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const configFilepath = "./config/config.yaml"
+const configFilepath = "./config.yaml"
 
 type HealthCheckConfig struct {
 	Port               int    `yaml:"healthcheck_port"`
